6988b1b8fa749003fb9188b6fbb110de3148f8de
Ashok Singhal <wuffusg@gmail.com>
2025-04-24 18:40:10 -0700

    Added Font_6x8

--- Drivers/SSD1306/fonts.h
+++ Drivers/SSD1306/fonts.h
@@ -13,8 +13,9 @@ typedef struct {
 } FontDef;
 
 //
-//  Export the 3 available fonts
+//  Export the 4 available fonts
 //
+extern FontDef Font_6x8;
 extern FontDef Font_7x10;
 extern FontDef Font_11x18;
 extern FontDef Font_16x26;
